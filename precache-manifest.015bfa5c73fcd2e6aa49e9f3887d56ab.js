4f2ea056548c5dbbaf8e5d1a93017affb9dec171
Venaz-dev <57592712+Venaz-dev@users.noreply.github.com>
2020-06-20 23:45:43 +0100

    Updates

---
+++ precache-manifest.015bfa5c73fcd2e6aa49e9f3887d56ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b861eeb4b9b3dacccba3efaf0756ca",
+    "url": "/salary/index.html"
+  },
+  {
+    "revision": "32ee27ea45ff2e965f05",
+    "url": "/salary/static/css/main.f254066e.chunk.css"
+  },
+  {
+    "revision": "c1809eb6275e1e7de2a0",
+    "url": "/salary/static/js/2.b234529b.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/salary/static/js/2.b234529b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32ee27ea45ff2e965f05",
+    "url": "/salary/static/js/main.0d132e31.chunk.js"
+  },
+  {
+    "revision": "0e2a797d2805a6d083bf",
+    "url": "/salary/static/js/runtime-main.23be8e35.js"
+  },
+  {
+    "revision": "d2d8a4182d963e7890d3aa482d319165",
+    "url": "/salary/static/media/github-sign-in.d2d8a418.jpg"
+  },
+  {
+    "revision": "e3094980ebdd7160ef9c5777b039f737",
+    "url": "/salary/static/media/time-is-money.e3094980.jpg"
+  }
+]);
